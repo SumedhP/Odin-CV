bddd6f180417581320beb5547341eaafc37678c1
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-12-04 16:23:24 -0800

    yeet

--- CV/world_position.py
+++ CV/world_position.py
@@ -1,16 +1,29 @@
 import cv2
-
+import numpy as np
 from Match import Match, Point
+
 
 # Goal is to take in a set of points on the screen and return the world position of the object using solvePnP
 
-camera_matrix = [
-    [1.0, 0.0, 0.0],
-    [0.0, 1.0, 0.0],
-    [0.0, 0.0, 1.0],
-]
 
-distortion_coefficients = [0.0, 0.0, 0.0, 0.0, 0.0]
+# import camera matrix and distortion coefficients from calibration
+CALIBRATION_PATH = "../camera_calibration_calibdb.json"
+
+# get the values from the calibration file
+import json
+with open(CALIBRATION_PATH, "r") as f:
+    calibration = json.load(f)
+
+camera_matrix = calibration["camera_matrix"]["data"]
+camera_matrix = np.array(camera_matrix).reshape((3, 3))
+
+distortion_coefficients = calibration["distortion_coefficients"]["data"]
+distortion_coefficients = np.array(distortion_coefficients).reshape((5, 1))
+
+print("Camera Matrix:\n", camera_matrix)
+print()
+print("Distortion matrix:\n", distortion_coefficients)
+
 
 # Actual plate size is 135mm x 135mm
 plate_size = 135
